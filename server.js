218de7267cdeb729b16c3f53b50fc8ed451a45cb
nhinguyen04 <valentinetwin94@gmail.com>
2023-04-21 18:24:20 -0700

    completed 1-4

--- server.js
+++ server.js
@@ -34,12 +34,73 @@ const server = http.createServer((req, res) => {
 
     /* ======================== ROUTE HANDLERS ========================== */
     // Phase 1: GET /
+    if (req.method === 'GET' && req.url === '/') {
+      const htmlPage = fs.readFileSync('./views/new-player.html', 'utf-8');
+      const availableRooms = world.availableRoomsToString();
+      const resBody = htmlPage.replace(/#{availableRooms}/g, `${availableRooms}`);
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      return res.end(resBody);
+    }
 
     // Phase 2: POST /player
+    if (req.method === 'POST' && req.url === ('/player')) {
+      const roomID = req.body.roomId;
+      const playerName = req.body.name;
+      const currentRoom = world.rooms[roomID];
+
+      player = new Player(playerName, currentRoom);
+
+      res.statusCode = 302;
+      res.setHeader('Location', `/rooms/${roomID}`);
+      return res.end();
+    }
 
     // Phase 3: GET /rooms/:roomId
+    if (req.method === 'GET' && req.url.startsWith('/rooms/')) {
+      const urlSplit = req.url.split('/');
+      const roomID = urlSplit[2];
 
-    // Phase 4: GET /rooms/:roomId/:direction
+      // redirect if player not in room
+      if (player.currentRoom.id !== Number(roomID)) {
+        res.statusCode = 302;
+        res.setHeader('Location', `/rooms/${player.currentRoom.id}`);
+        return res.end();
+      }
+
+      if (urlSplit.length === 3) {
+        const htmlPage = fs.readFileSync('./views/room.html', 'utf-8');
+        const room = world.rooms[roomID];
+
+        const resBody = htmlPage
+          .replace(/#{roomName}/g, `${room.name}`)
+          .replace(/#{roomId}/g, `${room.id}`)
+          .replace(/#{roomItems}/g, `${room.itemsToString()}`)
+          .replace(/#{exits}/g, `${room.exitsToString()}`)
+          .replace(/#{inventory}/g, `${player.inventoryToString()}`);
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        return res.end(resBody);
+      }
+
+      // Phase 4: GET /rooms/:roomId/:direction
+      if (urlSplit.length === 4) {
+        const direction = urlSplit[3];
+
+        try {
+          player.move(direction[0]);
+          res.statusCode = 302;
+          res.setHeader('Location', `/rooms/${player.currentRoom.id}`);
+          return res.end();
+        } catch (e) {
+          res.statusCode = 302;
+          res.setHeader('Location', `/rooms/${roomID}`);
+          return res.end();
+        }
+      }
+    }
 
     // Phase 5: POST /items/:itemId/:action
 
